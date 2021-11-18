e184dba8fc8fee633eacb49f97cfeb9451faf26f
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2021-11-18 11:39:52 +0600

    sidebar added

--- src/view/admin/adminmaster.vue
+++ src/view/admin/adminmaster.vue
@@ -177,44 +177,7 @@
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                    with font-awesome or any other icon font library -->
-                <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
-                    <p>
-                        Category
-                        <i class="right fas fa-angle-left"></i>
-                    </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                   
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>All Categories</p>
-                        </a>
-                    </li>
-                     <li class="nav-item">
-                    <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-th"></i>
-                    <p>
-                       Add Category
-                    </p>
-                    </a>
-                </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-th"></i>
-                    <p>
-                      other
-                    </p>
-                    </a>
-                </li>
-                </ul>
+                <compSidebar></compSidebar>
             </nav>
             <!-- /.sidebar-menu -->
             </div>
@@ -320,7 +283,7 @@ import("../../../node_modules/admin-lte/dist/js/adminlte.js");
 
 import('../../../node_modules/admin-lte/plugins/fontawesome-free/css/all.css');
 
-// import("../../../node_modules/admin-lte/dist/img");
+import compSidebar from './sidebar.vue';
 
 
 export default {
@@ -340,6 +303,9 @@ export default {
             localStorage.removeItem('AToken');
              this.$router.push('/admin/login');
         }
+    },
+    components:{
+        compSidebar
     }
     
 }
---
+++ src/view/admin/sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Category
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                   
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>All Categories</p>
+                        </a>
+                    </li>
+                     <li class="nav-item">
+                    <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                       Add Category
+                    </p>
+                    </a>
+                </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                      other
+                    </p>
+                    </a>
+                </li>
+                </ul>
+    </div>
+</template>
